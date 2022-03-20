edbb8bac1f2a2eca98692251378a99f3f297f92a
Kaique Lucena <kaiquefigueiredo01@hotmail.com>
2022-03-20 15:16:13 -0300

    fix: create_algs.sql

--- sql/create_algas.sql
+++ sql/create_algas.sql
@@ -1,11 +1,11 @@
 create table ranges(
-	ID int primary key auto_increment,
+	id int primary key auto_increment,
     inicio int not null,
     fim int not null
 );
 
 create table transactions(
-	ID int primary key auto_increment,
+	id int primary key auto_increment,
     tempo decimal(10,10),
     espaco int,
 	passo int not null,
