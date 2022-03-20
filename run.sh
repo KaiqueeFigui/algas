62726fdd07b6c1e4f8b34a0f8762a3483f3bf601
Kaique Lucena <kaiquefigueiredo01@hotmail.com>
2022-03-20 15:05:54 -0300

    add: Arquivo .env.example e correção create_algas.sql

--- run.sh
+++ run.sh
@@ -2,6 +2,3 @@ echo 'Iniciando container MySQL...'
 docker start algas_mysql_1
 echo 'Baixando e instalando dependências necessárias para o script...'
 pip3 install -r requirements.txt
-sleep 5
-echo 'Iniciando jupyter notebook...'
-jupyter notebook src/insert_data.ipynb --port 8889 --ip=
