8b978a3ff1413fc042f5455783a19b5f944a89f7
Victor Barbosa <victorbarbosa@victors-mbp.lan>
2022-04-01 20:51:05 -0300

    fix: pequenos ajustesgit add main.py

--- src/main.py
+++ src/main.py
@@ -13,7 +13,6 @@ def contador_tempo_memoria(inicio, fim, passo = 1):
     espaco = []
     start = time.time()
     for i in range(inicio, fim, passo):
-        print(i)
         transactions.append(i)
         espaco.append(sys.getsizeof(i))
         
@@ -21,6 +20,7 @@ def contador_tempo_memoria(inicio, fim, passo = 1):
         Transaction = TransactionModel(tempo = time.time() - start, espaco = espaco[i], passo = transactions[i], fk_range = Range.id)
         session.add(Transaction)
         session.commit()
+        print("Transaction: ", transactions[i], " - Memory: ", espaco[i])
 
 def select_range(inicio, fim, passo):
     range = session.query(RangeModel).filter_by(inicio = inicio, fim = fim, passo = passo).first()
@@ -32,11 +32,11 @@ def select_range(inicio, fim, passo):
     else:
         return range
 
-print("Escolha o iníco do range")
+print("Escolha o início do range")
 questionStart = int(input())
 print("Escolha o fim do range")
-questionEnd = int(input())
+questionFinal = int(input())
 print("Escolha o passo do range")
 questionStep = int(input())
 
-contador_tempo_memoria(questionStart, questionEnd, questionStep)
+contador_tempo_memoria(questionStart, questionFinal, questionStep)
