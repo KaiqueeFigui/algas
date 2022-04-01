9626b9411f1303b252a92765a48552865f344a18
Victor Barbosa <victorbarbosa@Victors-MacBook-Pro.local>
2022-03-31 21:54:15 -0300

    update: script run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
-echo 'Iniciando container MySQL...'
-docker start algas_mysql_1
-echo 'Baixando e instalando dependências necessárias para o script...'
-pip3 install -r requirements.txt
+echo 'Atualizando pacotes...'
+sudo apt-get update && sudo apt-get upgrade -y
+clear
+echo 'Instalando dependências necessárias...'
+sudo apt install docker.io -y
+clear
+sudo apt install docker-compose -y
+clear
+sudo apt install python3-pip -y
+clear
+
+echo 'Atualizando repositório...'
+git pull
+clear
+
+echo 'Criando arquivo para váriavel de ambiente ...'
+cat ./src/.env.example >> .env
+
+echo 'Iniciando docker compose...'
+sudo docker-compose up -d
