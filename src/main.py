dcd3089d90a37611555985f43c0e1860f92e29ef
Kalil Bego <kalil.bego@safra.com.br>
2022-04-24 15:41:52 -0300

    Inserindo transações por natureza

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import time
 import sys
+import random
+from datetime import date
 from database.models.RangeModel import RangeModel
 from database.models.TransactionModel import TransactionModel
+from database.models.NatureModel import NatureModel
 from database.Connection import Connection
+from tqdm import tqdm
 
 session = Connection().session
 
@@ -50,11 +54,39 @@ def valida_range(inicio, fim, passo):
     if passo > subtract:
         raise Exception("O passo deve ser menor para haver uma transação")
 
-print("Escolha o início do range")
-questionStart = int(input())
-print("Escolha o fim do range")
-questionFinal = int(input())
-print("Escolha o passo do range")
-questionStep = int(input())
+def gera_valores_por_natureza(data, natureza, quantidade):
+    for i in tqdm(range(0, quantidade), desc='Processando dados'):
+        random_transaction = round(random.uniform(1, 10_000), 2)
+        Nature = NatureModel(date = data, value = random_transaction, nature_type = natureza)
+        session.add(Nature)
+        session.commit()
 
-contador_tempo_memoria(questionStart, questionFinal, questionStep)
+def resgata_natureza(indice):
+    if indice == 1:
+        return "G2G"
+    elif indice == 2:
+        return "G2B"
+    elif indice == 3:
+        return "G2P"
+    elif indice == 4:
+        return "B2G"
+    elif indice == 5:
+        return "P2G"
+    elif indice == 6:
+        return "B2B"
+    elif indice == 7:
+        return "B2P"
+    elif indice == 8:
+        return "P2B"
+    elif indice == 9:
+        return "P2P"
+
+print("Digite a data no formato YYYY-MM-DD:")
+date_transaction = date.fromisoformat(input())
+print("\nEscolha a natureza: \n1 - G2G \n2 - G2B \n3 - G2P \n4 - B2G \n5 - P2G \n6 - B2B \n7 - B2P \n8 - P2B \n9 - P2P")
+nature_transaction = resgata_natureza(int(input()))
+print("\nDigite a quantidade de transações:")
+count_transactions = int(input())
+print("\n")
+
+gera_valores_por_natureza(date_transaction, nature_transaction, count_transactions)
