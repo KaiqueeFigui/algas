bec6202e1cbc2c57ca32a541bce3cc9eb953b4fe
Victor Barbosa <victor.barbosa@bandtec.com.br>
2022-04-25 12:50:44 -0300

    (refactor) main.py

--- src/main.py
+++ src/main.py
@@ -1,44 +1,6 @@
-import random
-from database.models.RangeModel import RangeModel
-from database.models.TransactionModel import TransactionModel
-from database.Connection import Connection
-
-session = Connection().session
-
-def generate_random_transactions(year):
-    verifyIfYearExistsInDatabase = session.query(TransactionModel).filter_by(year = year).first()
-
-    if (verifyIfYearExistsInDatabase == None):
-        for i in range(1, 5):
-            value = random.randint(300000000, 798061937)
-
-            if i == 4:
-                value = random.randint(600000000, 1098061937)
-            
-            Transaction = TransactionModel(year = year, description = str(year) + '-T' + str(i), value = value)
-            session.add(Transaction)
-            session.commit()
-            print("Ano: ", Transaction.year, "Ano/Trimestre ", Transaction.description, " - Valor: ", Transaction.value)
-    else:
-        print("\n")
-        transactionsExists = session.query(TransactionModel).filter_by(year = year).all()
-
-        for transaction in transactionsExists:
-            print("Ano: ", transaction.year, "Ano/Trimestre ", transaction.description, " - Valor: ", transaction.value)
-
-def generate_random_transactions_last_10_years():
-    for year in range(2022, 2022 - 10, -1):
-        generate_random_transactions(year)
-
-def get_all_transactions():
-    transactions = session.query(TransactionModel).all()
-
-    if len(transactions) == 0:
-        print("Nenhuma transação encontrada")
-    else:
-        print("\nTransações encontradas:\n")
-        for transaction in transactions:
-            print("Ano: ", transaction.year, "Ano/Trimestre ", transaction.description, " - Valor: ", transaction.value)
+from services.RandomTransactions import generate_random_transactions
+from services.RandomTransactions import generate_random_transactions_last_10_years
+from services.GetTransactions import get_all_transactions
 
 def main():
     print("Escolha a opção desejada:\n")
