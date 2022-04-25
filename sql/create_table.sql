97f8ffa5208928418ecbaedd668fd4f6844c29b9
Victor Barbosa <victor.barbosa@bandtec.com.br>
2022-04-25 20:05:06 -0300

    (update): script sql

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,10 +1,8 @@
-DROP TABLE IF EXISTS `transactions_with_card`;
-CREATE TABLE `transactions_with_card` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `year` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `value` varchar(255) DEFAULT NULL,
-  `space` int DEFAULT NULL,
-  `time` float(25,20)
-  PRIMARY KEY (`id`)
-) 
+create table transactions_with_card (
+	id int primary key auto_increment,
+    year varchar(255),
+    description varchar(255),
+    value varchar(255),
+    space int,
+    time float(25,20)
+);
